4fc5652bf4afc7fdff01798a011b663e40c11657
shubhamj974 <jagdaleshubham036@gmail.com>
2023-08-22 09:08:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './shared/components/sign-up/sign-up.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { FormComponent } from './shared/components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LoginComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
